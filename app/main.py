e5f2759acedc6e2b3b12f3e733574b2bb999fc1c
Konstantin Scheglov <kostai1210@mail.ru>
2023-11-04 22:10:00 +0300

    added endpoint DELETE

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db import crud
@@ -13,18 +13,29 @@ app = FastAPI()
 
 @app.post('/todos/', response_model=schemas.ReadTask)
 def create_task_API(task: schemas.DatabaseCreateTask, db: Session = Depends(get_db)):
-    new_task = crud.create_task(db=db, new_task=task) 
+    new_task = crud.create_task(db=db, new_task=task)
     return new_task
 
 
 @app.get('/todos/{todo_id}', response_model=schemas.ReadTask)
 def read_task_API(todo_id: int, db: Session = Depends(get_db)):
-    db_task = crud.get_task_by_id(db=db, id=todo_id)
-    return db_task
+    read_task = crud.get_task_by_id(db=db, id=todo_id)
+    if read_task is None:
+        raise HTTPException(status_code=404, detail="Задача не найдена")
+    return read_task
 
 
 @app.put('/todos/{todo_id}', response_model=schemas.UpdateTask)
 def update_todo(todo_id: int, new_task: schemas.UpdateTask, db: Session = Depends(get_db)):
     upd_task = crud.update_task(db=db, task_id=todo_id, new_task=new_task)
     return upd_task
+
+@app.delete('/todos/{todo_id}', response_model=str)
+def delete_task(todo_id: int, db: Session = Depends(get_db)):
+    del_task = crud.get_task_by_id(db=db, id=todo_id)
+    if del_task is None:
+        raise HTTPException(status_code=404, detail="Задача не найдена")
+    db.delete(del_task)
+    db.commit()
+    return "Задача успешно удалена"
 
